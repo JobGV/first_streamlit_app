ab5a24e39779762bcd563c798237e1603e448386
Job García <98175358+JobGV@users.noreply.github.com>
2023-06-13 16:02:33 -0600

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Bluberry Oatmeal")
-stremlit.text("Kale, Spinach  Rocket Smoothie")
-stremlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text("Kale, Spinach  Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
